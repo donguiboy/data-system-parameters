8c453c3116f1eb3343b0be9fca40fd1d58f84a11
gonzalo lara <gonzaloflara@gmail.com>
2023-02-23 14:35:05 -0300

    ejercicio resuelto con ifs (creo que muchos

--- calc.py
+++ calc.py
@@ -3,10 +3,13 @@ import sys
 
 
 def main():
-    """Implement the calculator"""
-    pass  # YOUR CODE HERE
-
+    if sys.argv[2] == "+":
+        return int(sys.argv[1]) + int(sys.argv[3])
+    if sys.argv[2] == "-":
+        return int(sys.argv[1]) - int(sys.argv[3])
+    if sys.argv[2] == "*":
+        return int(sys.argv[1]) * int(sys.argv[3])
+    return None
 
 if __name__ == "__main__":
     print(main())
-    
